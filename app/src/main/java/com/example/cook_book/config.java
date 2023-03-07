79d5eba763b93837bf92dc3e9936c72bedfcb147
IshaniGodhani <121079542+IshaniGodhani@users.noreply.github.com>
2023-03-07 10:00:08 -0800

    Initial commit

---
+++ app/src/main/java/com/example/cook_book/config.java
@@ -0,0 +1,9 @@
+package com.example.cook_book;
+
+import android.os.Environment;
+
+import java.io.File;
+
+public class config {
+    public static File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+}
